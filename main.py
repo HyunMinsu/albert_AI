9120c68710d88b03d7c500f8a2a3604780f4c2ea
Hyun, Minsu <hyunminsunotes@gmail.com>
2021-11-16 16:42:56 +0900

    first flask code

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import pandas as pd
+from flask import Flask, request, json
+import random
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+
+commonResponse = {
+    'version': '2.0',
+    'resultCode': 'OK',
+    'output': {}
+}
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+df = pd.read_csv("bbanglist.csv", encoding='cp949')
+list_df = df.values.tolist()
+num=0
+# for data in list_df:
+#     bbang_shop_name = list_df[num][0]
+#     bbang_shop_best = list_df[num][1]
+#     bbang_shop_time = list_df[num][2]
+#     bbang_shop_address = list_df[num][3]
+#     num = num + 1
+
+for data in list_df:
+    print(list_df[num][0])
+    print(list_df[num][1])
+    print(list_df[num][2])
+    print(list_df[num][3])
+    num = num + 1
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route('/')
+def index():
+    return 'Hello Flask'
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route('/getlistofshop', methods=['POST'])
+def getshoplist():
+    response = commonResponse
+
+    response['output']['bbang_shop_name'] = bbang_shop_name
+    response['output']['bbang_shop_time'] = bbang_shop_best
+    response['output']['bbang_shop_best'] = bbang_shop_time
+    response['output']['bbang_shop_address'] = bbang_shop_address
+    print(response)
+    return json.dumps(response)
+
+# print(bbang_shop_name)
+# print(bbang_shop_address)
+# print(bbang_shop_time)
+# print(bbang_shop_best)
